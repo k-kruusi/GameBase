a0aaeee06841760f3082ca36f914f7fb4e748990
Juan Toro <jdtoro1119@gmail.com>
2018-02-28 17:06:03 -0500

    Created empty x code project

---
+++ SwiftDungeon/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  SwiftDungeon
+//
+//  Created by Toro Juan D. on 2/28/18.
+//  Copyright © 2018 Toro Juan D. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+}
---
+++ SwiftDungeon/GameViewController.swift
@@ -0,0 +1,26 @@
+//
+//  GameViewController.swift
+//  SwiftDungeon
+//
+//  Created by Toro Juan D. on 2/28/18.
+//  Copyright © 2018 Toro Juan D. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let scene = GameScene(size: CGSize(width: 2048, height: 1536))
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+    }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
