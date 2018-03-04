c2854739417a97fd745d54dcef10a9f5c5180f1d
kuroChanh <tuyetnhi@hotmail.ca>
2018-03-04 15:42:37 -0500

    Created the new Xcode project and removed the .sks files

---
+++ QuackNBOOM/QuackNBOOM/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  QuackNBOOM
+//
+//  Created by Ngo Tuyetnhi N. and Benoit Neriah on 3/4/18.
+//  Copyright © 2018 Neriah and Neenee. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+}
