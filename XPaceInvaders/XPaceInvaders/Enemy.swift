a32131664ab8bc716712d202dab5a1cce951b773
f0rmality <n01133376@nl-00116-026.mediastudies.ca>
2018-03-08 16:32:18 -0500

    Base classes

---
+++ XPaceInvaders/XPaceInvaders/Enemy.swift
@@ -0,0 +1,13 @@
+//
+//  BaseEnemy.swift
+//  XPaceInvaders
+//
+//  Created by Mazza Matthew J. on 3/8/18.
+//  Copyright © 2018 Fonseca Barbalho Talis. All rights reserved.
+//
+
+import Foundation
+
+class Enemy : BaseGameObject {
+    //put stuff here
+}
