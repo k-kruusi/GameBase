e90cb5dcf9864723460972595fcbe787368fe1ce
Haifeng Su <spy.haifeng@gmail.com>
2018-03-12 19:14:20 -0400

    Update on Space-X Space Program
    -added background
    -added player (Spaceship)
    -added trails
    -added projectiles (almost done)
    -added player controls

--- Space-X Defense Program/Space-X Defense Program/GameScene.swift
+++ Space-X Defense Program/Space-X Defense Program/GameScene.swift
@@ -11,63 +11,87 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    //hello world
+    private let background = SKSpriteNode(imageNamed: "background")
+    private let player = Player()
+    let projectiles = Projectile()
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
     
+    var gameTimer: Timer!                     //timer for repeating functions
+    
     override func didMove(to view: SKView) {
         
+        backgroundColor = SKColor.black       //defaulted colored to black for the background
+        background.zPosition = -1             //depth of background, -1 makes it go behind other 2D elements
+        background.position = CGPoint(x: size.width / 2, y: size.height / 2 )          //centering the background images
+        background.setScale(1.43)             //Maximizing the pixel resolution for the 1080p
+        addChild(background)                  //adding the background to the scene
+        
+        addChild(player)                      //adding the spaceship to the scene
+        player.position = CGPoint(x: 220 , y: size.height/2)              //positioning the spaceship to spawn on the centre of the leftmost corner
+        player.setScale(0.6)                  //scale the spaceship down
+        
+        //addChild(projectiles)
+        
+        
+        
         // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
+        /*self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
         if let label = self.label {
             label.alpha = 0.0
             label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        }*/
         
         // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        let w = (self.size.width + self.size.height) * 0.02
+        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.2) //0.3 default
         
         if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+            spinnyNode.lineWidth = 50  //2.5 default
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
+            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 3)))  //1 default
+            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.05),                                //0.5 default i think
+                                              SKAction.fadeOut(withDuration: 0.2),                             //0.5 default
                                               SKAction.removeFromParent()]))
         }
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
+        let projectiles = Projectile()
+        addChild(projectiles)
+        projectiles.position.x = 220
+        projectiles.position.y = pos.y
+        
     }
     
+    //This function tracks the movement of the touch input for the spaceship
     func touchMoved(toPoint pos : CGPoint) {
         if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
+            n.position.y = pos.y
+            n.strokeColor = SKColor.yellow
             self.addChild(n)
+  
+            //limiting the Y of the thrusters to match the ships
+            if(n.position.y > 1420){n.position.y = 1420}
+            if(n.position.y < 120){n.position.y = 120}
         }
+        
+        //moving the ship to match the touch movement
+        player.position.y = pos.y
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        
+        
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(Thruster) , userInfo: nil, repeats: true)
+        
         
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
@@ -77,6 +101,9 @@ class GameScene: SKScene {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        gameTimer.invalidate()
+        
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
@@ -84,8 +111,26 @@ class GameScene: SKScene {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
+    @objc func Thruster()
+    {
+        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
+            n.position.y = player.position.y
+            n.strokeColor = SKColor.yellow
+            self.addChild(n)
+        }
+    }
+    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        projectiles.position.x += 20
+        
+        //limiting the spaceship from going too high and too low on the screen
+        if(player.position.y < 120){player.position.y = 120}
+        if(player.position.y > 1420){player.position.y = 1420}
+        
     }
+    
+   
 }
