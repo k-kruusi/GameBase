9cb5844b17252be46f0901052c60378f6a5b55db
Keyyva <keyyva@gmail.com>
2018-03-14 17:26:49 -0400

    Started development on the AudioPlayer class

---
+++ QuackNBOOM/QuackNBOOM/AudioPlayer.swift
@@ -0,0 +1,28 @@
+//
+//  AudioPlayer.swift
+//  QuackNBOOM
+//
+//  Created by Benoit Neriah R. on 3/14/18.
+//  Copyright © 2018 Neriah and Neenee. All rights reserved.
+//
+
+import Foundation
+
+final class AudioPlayer {
+    static let sharedInstance: AudioPlayer = AudioPlayer()
+    
+    // TODO: Create public Enum of soundeffects to play
+    
+    
+    // Initialization of the AudioPlayer
+    //
+    fileprivate init() {
+        
+    }
+    
+    
+    // TODO: Create a public function that plays the sound effects specified
+    //      from the enum entered into the parameter
+    
+    //public void playSoundEffect(SoundEffects soundEffect) { // Where SoundEffects is an Enum
+}
