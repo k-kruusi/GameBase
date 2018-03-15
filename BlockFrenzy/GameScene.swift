fd8fb6d889c6252d59abdbe4ac903322f1937292
Rodrigues Isaac <>
2018-03-15 18:06:36 -0400

    got pad working music works with inside game.

--- BlockFrenzy/GameScene.swift
+++ BlockFrenzy/GameScene.swift
@@ -18,6 +18,8 @@ class GameScene: SKScene {
     
     
     var BackgroundMusic = AVAudioPlayer()
+    var fingerIsOnpaddle = false
+    
     
     
     override init(size: CGSize) {
@@ -37,7 +39,38 @@ class GameScene: SKScene {
             backgoundImage.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
             self.addChild(backgoundImage)
             
+            self.physicsWorld.gravity = CGVector(dx: 0, dy: 0)
             
+            let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+            self.physicsBody = border
+            self.physicsBody?.friction = 0
+            
+            let ball = SKSpriteNode(imageNamed: "Ball")
+            ball.name = BallCatName
+            ball.position = CGPoint(x: self.frame.size.width/4, y: self.frame.size.width/4)
+            self.addChild(ball)
+            ball.setScale(2)
+            
+            
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.frame.size.width/2)
+            ball.physicsBody?.friction = 0
+            ball.physicsBody?.restitution = 1
+            ball.physicsBody?.linearDamping = 0
+            ball.physicsBody?.allowsRotation = false
+            
+            ball.physicsBody?.applyImpulse(CGVector(dx: 2, dy: -2))
+            
+            let paddle = SKSpriteNode(imageNamed: "paddle")
+            paddle.name = paddleCatName
+            paddle.position = CGPoint(x: self.frame.midX, y: paddle.frame.size.height * 2)
+            
+            self.addChild(paddle)
+            paddle.setScale(1.5)
+
+            paddle.physicsBody = SKPhysicsBody(rectangleOf: paddle.frame.size)
+            paddle.physicsBody?.friction = 0.4
+            paddle.physicsBody?.restitution = 0.1
+            paddle.physicsBody?.isDynamic = false
             
             
             
@@ -46,6 +79,56 @@ class GameScene: SKScene {
         }
         
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+  
+    
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let touchLocation = touch.location(in: self)
+        let body:SKPhysicsBody? = self.physicsWorld.body(at: touchLocation)
+        
+        if body?.node?.name == paddleCatName {
+            print("paddle touched")
+            fingerIsOnpaddle = true
+        }
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if fingerIsOnpaddle {
+            guard let touch = touches.first else {
+                return
+            }
+            let touchLoc = touch.location(in: self)
+            let prevTouchLocation = touch.previousLocation(in: self)
+            
+            let paddle = self.childNode(withName: paddleCatName) as! SKSpriteNode
+            
+            var newXPos = paddle.position.x + (touchLoc.x - prevTouchLocation.x)
+            
+            
+            
+            newXPos = max(newXPos,paddle.size.width / 2)
+            newXPos = min(newXPos, self.size.width - paddle.size.width / 2)
+            
+            paddle.position = CGPoint(x: newXPos, y: paddle.position.y)
+            
+        }
+        
+        
+    }
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        fingerIsOnpaddle = false
+        
+    }
+    
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
