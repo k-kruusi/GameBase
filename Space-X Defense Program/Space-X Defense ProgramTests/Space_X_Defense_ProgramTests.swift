43d0177b07abb974d0307face3501898431f6332
Favero Miguel Fernando <miguel.favero@hotmail.co.uk>
2018-03-20 14:11:30 -0400

    Pushing enemies in.

---
+++ Space-X Defense Program/Space-X Defense ProgramTests/Space_X_Defense_ProgramTests.swift
@@ -0,0 +1,36 @@
+//
+//  Space_X_Defense_ProgramTests.swift
+//  Space-X Defense ProgramTests
+//
+//  Created by Favero Miguel Fernando on 3/13/18.
+//  Copyright © 2018 Miguel Haifeng. All rights reserved.
+//
+
+import XCTest
+@testable import Space_X_Defense_Program
+
+class Space_X_Defense_ProgramTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
