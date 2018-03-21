585d7b04d5e166277230524b3b0dd2ce82d77de5
Favero Miguel Fernando <miguel.favero@hotmail.co.uk>
2018-03-21 16:58:10 -0400

    finished game - Miguel and Haifeng

---
+++ Space-X Defense Program/Space-X Defense Program/GameOverScene.swift
@@ -0,0 +1,47 @@
+//
+//  GameOverScene.swift
+//  Space-X Defense Program
+//
+//  Created by Favero Miguel Fernando on 3/21/18.
+//  Copyright © 2018 Su Haifeng. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class GameOverScene: SKScene {
+    
+    init(size: CGSize, won:Bool) {
+        
+        super.init(size: size)
+        
+        backgroundColor = SKColor.white
+        
+        let message = won ? "You Won!" : "You Lose :["
+        
+        let label = SKLabelNode(fontNamed: "Chalkduster")
+        label.text = message
+        label.fontSize = 40
+        label.fontColor = SKColor.black
+        label.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(label)
+        
+        // 4
+        run(SKAction.sequence([
+            SKAction.wait(forDuration: 3.0),
+            SKAction.run() {
+                // 5
+                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+                let scene = GameScene(size: size)
+                self.view?.presentScene(scene, transition:reveal)
+            }
+            ]))
+        
+    }
+    
+    // 6
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
