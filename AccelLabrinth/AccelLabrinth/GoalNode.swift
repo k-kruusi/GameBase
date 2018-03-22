f0b073de169dd52d447b02b88cd1c0061b8e6767
Darren Rasmussen <brydar@Rogers.com>
2018-03-22 16:23:34 -0400

    Marya's Sprites and start of adding goal and ending of game
    all the assets are Marya work as I begin to work on the victory contitions while Marya works on gameplay

---
+++ AccelLabrinth/AccelLabrinth/GoalNode.swift
@@ -0,0 +1,14 @@
+//
+//  GoalNode.swift
+//  AccelLabrinth
+//
+//  Created by Rasmussen Darren K. on 3/22/18.
+//  Copyright © 2018 Darren/Marya. All rights reserved.
+// made to handle hit detection between itself and the player
+
+import Foundation
+import SpriteKit
+
+class GoalObject: GameObject {
+    
+}
